eea196b35e2b336aacdd41934d822c6a0b770512
Tomás Sousa <118855300+TommySousa@users.noreply.github>
2023-01-02 14:08:50 +0000

    Added a span that tells the user if the passwords match or not

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const password = document.getElementById("password");
 const cPassword = document.getElementById("cPassword");
+const message = document.getElementById("message");
 
 const check = () => {
     if(password.value !== "" || cPassword.value !== ""){
@@ -8,11 +9,15 @@ const check = () => {
             cPassword.classList.add('error');
             cPassword.classList.remove('success');
             password.classList.remove('success');
+            message.innerHTML = "Passwords do NOT match";
+            message.style.color = "Red";
         } else if(password.value === cPassword.value) {
             password.classList.add('success');
             cPassword.classList.add('success');
             cPassword.classList.remove('error');
             password.classList.remove('error');
+            message.innerHTML = "Passwords match";
+            message.style.color = "Green";
         } 
     }
 };
